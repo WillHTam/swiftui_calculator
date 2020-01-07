faf7459930953adb35d8c3332fba0aa50cf9f016
William T <william.tam18@gmail.com>
2020-01-07 14:49:51 +0800

    Initial Commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by William on 1/7/20.
+//  Copyright © 2020 William T. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
